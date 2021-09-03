cf71b1722c30847feee64fded9031194ca83d345
Furkan Hanci <furkanhanci265@gmail.com>
2021-09-03 22:11:40 +0300

    MainStoryBoard deleted, new UIColor extension added.

---
+++ HistoriX/HistoriX/Extensions/UIColor+Ext.swift
@@ -0,0 +1,13 @@
+//
+//  UIColor+Ext.swift
+//  HistoriX
+//
+//  Created by Furkan Hanci on 9/3/21.
+//
+
+import UIKit
+
+
+extension UIColor {
+    static let mainBackground = UIColor(red: 214 / 250, green: 148 / 250, blue: 73 / 250, alpha: 1)
+}
