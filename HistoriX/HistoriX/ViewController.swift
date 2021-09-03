85689a2de66736ac52c71365b02f57f4050a7552
Furkan Hanci <87238473+FurkanHanciSecond@users.noreply.github.com>
2021-09-03 21:59:40 +0300

    First Commit
    Project has been uploaded

---
+++ HistoriX/HistoriX/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HistoriX
+//
+//  Created by Furkan Hanci on 9/3/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
